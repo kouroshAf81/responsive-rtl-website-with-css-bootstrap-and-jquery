62ec46a633798816ecdbf9fec0aa4d42d55ae0b4
kourosh af <kouroshaf81@gmail.com>
2023-08-04 10:29:24 +0430

    responsive rtl website with bootstrap and jquery

---
+++ js/script.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function(){
+    "use strict";
+    $('#slider-carousel').carouFredSel({
+        responsive:true,
+        width:'100%',
+        circular:true,
+        scroll:{
+            items:1,
+            duration:500,
+            pauseOnHover:true
+        },
+        auto:true,
+        items:{
+            visible:{
+                min:1,
+                max:1
+            },
+            height:"variable"
+        },
+        pagination:{
+            container:".sliderpager",
+            pageAnchorBuilder:false
+        }
+    });
+});
+$(window).scroll(function(){
+    var top = $(window).scrollTop();
+    if(top>=60){
+        $('header').addClass('secendery')
+    }
+    else if($('header').hasClass('secendery')){
+        $('header').removeClass('secendery');
+    }
+});
